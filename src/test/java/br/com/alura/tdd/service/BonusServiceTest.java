6bfead56cea115e54a535bf1dc327dd5b18df25f
Maycon Rabelo <loganrabelo@gmail.com>
2021-09-02 10:38:08 -0300

    testes unitarios

---
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,38 @@
+package br.com.alura.tdd.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
+class BonusServiceTest {
+
+	@Test
+	void bonusDeveriaSerZeroParaFuncionariosComSalarioMuitoAlto() {
+		BonusService service = new BonusService();
+
+		assertThrows(IllegalArgumentException.class,
+				() -> service.calcularBonus(new Funcionario("maycon", LocalDate.now(), new BigDecimal("25000"))));
+	}
+	
+	@Test
+	void bonusDeveriaSer10PorCentoDoSalario() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("maycon", LocalDate.now(), new BigDecimal("2500")));
+		
+		assertEquals(new BigDecimal("250.00"), bonus);
+	}
+	
+	@Test
+	void bonusDeveriaSer10PorCentoParaSalarioDeExatamente1000() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("maycon", LocalDate.now(), new BigDecimal("10000")));
+		
+		assertEquals(new BigDecimal("1000.00"), bonus);
+	}
+
+}
---
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -0,0 +1,45 @@
+package br.com.alura.tdd.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+
+public class ReajusteServiceTest {
+
+	private ReajusteService service;
+	private Funcionario funcionario;
+	
+	@BeforeEach
+	public void inicializar() {
+		service = new ReajusteService();
+		funcionario = new Funcionario("joao", LocalDate.now(), new BigDecimal("1000.00"));
+	}
+
+	@Test
+	public void reajusteDeveriaSerDeTresPorCentoQuandoDesempenhoForADesejar() {
+		service.concederReajuste(funcionario,Desempenho.A_DESEJAR);
+		
+		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
+	}
+	
+	@Test
+	public void reajusteDeveriaSerDeQuinzePorCentoQuandoDesempenhoForBom() {
+		service.concederReajuste(funcionario,Desempenho.BOM);
+		
+		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
+	}
+	
+	@Test
+	public void reajusteDeveriaSerDeVintePorCentoQuandoDesempenhoForOtimo() {
+		service.concederReajuste(funcionario,Desempenho.OTIMO);
+		
+		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
+	}
+}
